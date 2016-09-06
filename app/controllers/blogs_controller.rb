e04b39714e1c7361b187a52653245200ecb82536
HoangQuan <quandhcn@gmail.com>
2016-09-06 09:48:00 +0700

    first setup

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,26 @@
+class BlogsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def edit
+  end
+
+  def update
+    
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  private
+
+  def post_params
+    
+  end
+end
